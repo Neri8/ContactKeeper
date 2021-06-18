2a2f29202208f442fc3acdc3d719fc3bef38beda
Nerius <75215644+Neri8@users.noreply.github.com>
2021-06-18 21:37:28 +0200

    Initial commit

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const user = require('../models/User');
+
 // @route  POST api/users
 // @desc   Register a user
 // @access Public
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 
 const express = require('express');
+const connectDB = require('./config/db');
+
+// connect database
+
+connectDB();
 
 const app = express();
 
